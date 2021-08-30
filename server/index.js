5e54d327dc4269e45630cea35ae82f7772f9b321
Camara <victor.camara@ksgestao.com.br>
2021-08-30 18:10:27 -0300

    feat: primeira versão do app peer

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const server = require("http").createServer((request, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.write("Hello World!");
+  res.end();
+});
+
+const socketIo = require("socket.io");
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    credentials: false,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connection", socket.id);
+  socket.on("join-room", (roomId, userId) => {
+    // adiciona os usuarios na mesma sala
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      console.log("disconnected!", roomId, userId);
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
+const startServer = () => {
+  const { address, port } = server.address();
+  console.info(`app running at ${address}:${port}`);
+};
+
+server.listen(process.env.PORT || 3000, startServer);
